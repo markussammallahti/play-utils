d55bcda397ecb15c22d94d62af6ec988a73bdda2
Markus Sammallahti <markus@sammallahti.fi>
2021-05-05 23:00:47 +0300

    Use JsPath in property matcher

--- src/main/scala/mrks/play/test/JsonPropertyMatchers.scala
+++ src/main/scala/mrks/play/test/JsonPropertyMatchers.scala
@@ -1,29 +1,35 @@
 package mrks.play.test
 
 import org.scalatest.matchers.{HavePropertyMatchResult, HavePropertyMatcher}
-import play.api.libs.json.{Format, JsNull, JsValue, Json}
+import play.api.libs.json.{Format, JsNull, JsPath, JsValue, Json}
 
+import scala.language.implicitConversions
 
 trait JsonPropertyMatchers {
-  protected def property[A : Format](key: String, expected: A): HavePropertyMatcher[JsValue, JsValue] = HavePropertyMatcher { json =>
-    val value = (json \ key).getOrElse(JsNull)
+  implicit def intToJsPath(value: Int): JsPath       = JsPath \ value
+  implicit def stringToJsPath(value: String): JsPath = JsPath \ value
+
+  protected def property[A : Format](path: JsPath, expected: A): HavePropertyMatcher[JsValue, JsValue] = HavePropertyMatcher { json =>
+    val value = path.asSingleJson(json).getOrElse(JsNull)
 
     HavePropertyMatchResult(
       value.validate[A].fold(_ => false, _ == expected),
-      key,
+      toPropertyName(path),
       Json.toJson(expected),
       value
     )
   }
 
-  protected def property(key: String): HavePropertyMatcher[JsValue, Boolean] = HavePropertyMatcher { json =>
-    val value = (json \ key).isDefined
+  protected def property(path: JsPath): HavePropertyMatcher[JsValue, Boolean] = HavePropertyMatcher { json =>
+    val value = path.asSingleJson(json).isDefined
 
     HavePropertyMatchResult(
       value,
-      key,
+      toPropertyName(path),
       true,
       value
     )
   }
+
+  private def toPropertyName(path: JsPath) = path.path.foldLeft("")((acc, p) => acc + p.toJsonString)
 }
